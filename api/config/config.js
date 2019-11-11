d9dcc3ace5b38cc473be95774ac556979ea75fcf
John Akins <JohnAkins.dev@gmail.com>
2019-11-11 07:22:40 +0100

    setup travis CI 3

--- api/config/config.js
+++ api/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
 
   test: {
     database: 'teamwork_test',
-    username: 'postgres',
+    username: 'test',
     password: 'root',
     host: '127.0.0.1',
     dialect: 'postgres'
